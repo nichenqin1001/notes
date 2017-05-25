96ca05284894a5ef83976eb93811dcb380cd5981
nichenqin1001 <nichenqin@hotmail.com>
2017-05-25 15:52:07 +0800

    select noe

--- client/components/Note/NoteListEmptyItem.js
+++ client/components/Note/NoteListEmptyItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NoteListEmptyItem = () => {
   return (
     <div>
-      <h5>暂无笔记</h5>
+      <h3>暂无笔记</h3>
       <p>点击添加笔记开始记录</p>
     </div>
   );
--- client/components/Note/NoteListItem.js
+++ client/components/Note/NoteListItem.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
+import { Link, withRouter } from 'react-router-dom';
 
-const NoteListItem = (props) => {
-  return (
-    <div>
-      <h5>{props.note.title || 'Untitled Note'}</h5>
-      <p>{moment(props.note.createAt).format('YYYY-MM-DD')}</p>
-      <button onClick={() => Meteor.call('notes.remove', props.note)}>删除笔记</button>
-    </div>
-  );
-};
+class NoteListItem extends Component {
+  render() {
+    const { note, match } = this.props;
+    return (
+      <div>
+        <Link to={`${match.path}/${note._id}`}>
+          <h3>{note.title || '未命名笔记'}</h3>
+          <p>{moment(note.createAt).format('YYYY-MM-DD')}</p>
+          <button onClick={() => Meteor.call('notes.remove', note)}>删除笔记</button>
+        </Link>
+      </div>
+    );
+  }
+}
 
 NoteListItem.propTypes = {
   note: PropTypes.object.isRequired
 };
+
+NoteListItem = withRouter(NoteListItem);
 
 export default NoteListItem;
