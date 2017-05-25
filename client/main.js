a7d0c90bc034a63c2c2da6104589d03b73eef59f
nichenqin1001 <nichenqin@hotmail.com>
2017-05-25 23:36:55 +0800

    removed redux

--- client/main.js
+++ client/main.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import reducer from './reducers';
 
 import Routes from './routes/routes';
 import '../imports/collections/notes';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
 Meteor.startup(() => {
   ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducer)}>
-      <Routes />
-    </Provider>,
+    <Routes />,
     document.getElementById('app'));
 });
